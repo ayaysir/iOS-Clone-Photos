e874499599f7eb434bf1e5a35397cda3cab7fefd
윤범태 <yoonbumtae@gmail.com>
2024-08-11 18:32:17 +0900

    Design: SearchView 추가

--- Pixo-Assignment-Photos/View/ForYouView.swift
+++ Pixo-Assignment-Photos/View/ForYouView.swift
@@ -8,19 +8,19 @@
 import SwiftUI
 
 struct ForYouView: View {
-    var body: some View {
-      VStack {
-        Text("No Content")
-          .font(.title)
-          .bold()
-        Text("Photos scans your library to offer content curated for you. Connect your iPhone to power overnight to continue scanning.")
-          .foregroundStyle(.gray)
-          .multilineTextAlignment(.center)
-      }
-      .padding()
-      .navigationBarTitleDisplayMode(.inline)
-      .navigationTitle("For You")
+  var body: some View {
+    VStack {
+      Text("No Content")
+        .font(.title)
+        .bold()
+      Text("Photos scans your library to offer content curated for you. Connect your iPhone to power overnight to continue scanning.")
+        .foregroundStyle(.gray)
+        .multilineTextAlignment(.center)
     }
+    .padding()
+    .navigationBarTitleDisplayMode(.inline)
+    .navigationTitle("For You")
+  }
 }
 
 #Preview {
---
+++ Pixo-Assignment-Photos/View/SearchView.swift
@@ -0,0 +1,43 @@
+//
+//  SearchView.swift
+//  Pixo-Assignment-Photos
+//
+//  Created by 윤범태 on 8/11/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+  @State private var searchText = ""
+  
+  var body: some View {
+    VStack {
+      if searchText.isEmpty {
+        Text("No Suggestions")
+          .font(.title)
+          .bold()
+        Text("Photos scans your library to offer Search Suggestions. Connect your iPhone to power overnight to continue scanning.")
+          .foregroundStyle(.gray)
+          .multilineTextAlignment(.center)
+      } else {
+        Text("No Results")
+          .font(.title)
+          .bold()
+        Text("There were no results for \"\(searchText).\"\nTry a new search.")
+          .foregroundStyle(.gray)
+          .multilineTextAlignment(.center)
+      }
+      
+    }
+    .padding()
+    .navigationBarTitleDisplayMode(.inline)
+    .navigationTitle("Search")
+    .searchable(text: $searchText)
+  }
+}
+
+#Preview {
+  NavigationView {
+    SearchView()
+  }
+}
